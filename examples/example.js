0d58466608fde4cc2fa3441fc15a7e08650a3dff
Vadim M. Bryshev <vadimbaryshev@gmail.com>
2012-07-15 16:38:48 +0400

    Small optimization

--- examples/example.js
+++ examples/example.js
@@ -1,14 +1,15 @@
 var form = require('./../index');
 
 var fields = {
+	name: [
+		form.filter(form.Filter.trim),
+		form.validator(form.Validator.notEmpty, 'Empty name'),
+		form.validator(form.Validator.is, /[0-9]+/, 'Bad name')
+	],
 	text: [
 		form.filter(form.Filter.trim),
 		form.validator(form.Validator.notEmpty, 'Empty text'),
 		form.validator(form.Validator.len, 30, 1000, 'Bad text length')
-	],
-	name: [
-		form.filter(form.Filter.trim),
-		form.validator(form.Validator.notEmpty, 'Empty name')
 	]
 };
 
